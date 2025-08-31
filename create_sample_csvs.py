a5e9ba6fcdde0091377139e2313c8811f3e9ceec
GauravSingh-G <gaurav6ngh@gmail.com>
2025-08-31 14:15:30 +0530

    Initial commit: Added Gesture to Voice Translator project files

---
+++ create_sample_csvs.py
@@ -0,0 +1,18 @@
+﻿import csv
+
+header = [f'x{i}' for i in range(21)] + [f'y{i}' for i in range(21)] + [f'z{i}' for i in range(21)] + ['label']
+
+row_a = [0.0]*63 + ['A']
+row_b = [0.1]*63 + ['B']
+
+with open('A_data.csv','w',newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerow(row_a)
+
+with open('B_data.csv','w',newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerow(row_b)
+
+print("Created A_data.csv and B_data.csv (sample files).")
